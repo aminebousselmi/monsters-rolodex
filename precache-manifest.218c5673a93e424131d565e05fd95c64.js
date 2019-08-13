3cdb1cc83b81726ec6b55abe3486dfc3e9d280ef
System Administrator <root@MacBook-Pro-de-Bousselmi.local>
2019-08-13 18:50:54 +0200

    Updates

---
+++ precache-manifest.218c5673a93e424131d565e05fd95c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112f46c275ccb14c84ba3287e4bdd6ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ebe1eab5b16e499eff54",
+    "url": "/monsters-rolodex/static/css/main.a7edda50.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "ebe1eab5b16e499eff54",
+    "url": "/monsters-rolodex/static/js/main.8bf69cc5.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
